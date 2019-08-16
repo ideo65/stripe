c21495d5c49830398788de06f17c343e24b15178
ideo65 <52700001+ideo65@users.noreply.github.com>
2019-08-16 14:43:45 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -10,22 +10,28 @@ $event = json_decode($postdata);
 // Handle the event
 switch ($event->type) {
   case 'invoice.created':
-    $invoice_sub_id = $event->data->object->lines->data[0]->plan->id;
     $invoice_id = $event->data->object->id;
-    $plans = array('abonnement_1','abonnement_2');
-    if (in_array( $invoice_sub_id, $plans )) {
-      \Stripe\InvoiceItem::update(
-        $invoice_id,
+    $invoice_obj = \Stripe\Invoice::retrieve($invoice_id);
+    $plan_id = $invoice_obj->lines->data[0]->plan->id;
+    $sub_id = $invoice_obj->subscription;
+
+    $plans = array( 'abonnement1' , 'abonnement2' );
+    if (in_array( $plan_id, $plans )) {
+      \Stripe\Subscription::update(
+        $sub_id,
         [
-          'tax_rates' => [
-            'txr_1CflvFTnKg4V6yrg90ry5foU',
+          'default_tax_rates' => [
+            'txr_1CflvCBnKg4V6yrg90ry5foU',
           ],
         ]
       );
     } else {
-  }
+    }
+
   break;
+
   default:
+    // Unexpected event type
     http_response_code(400);
     exit();
 }
